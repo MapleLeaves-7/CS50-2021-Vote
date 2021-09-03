bcaf5a25b336e50f1e5d6fac54a2c4340ec49eec
MapleLeaves <64523738+MapleLeaves-7@users.noreply.github.com>
2021-09-03 19:04:00 +0800

    Add database models

--- website/__init__.py
+++ website/__init__.py
@@ -1,9 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+DB_NAME = "voting.db"
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "asdfasdfasdf asdfasdfasdf"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///{DB_NAME}'
+    db.init__app(app)
 
     from .views import views
     from .auth import auth
---
+++ website/models.py
@@ -0,0 +1,41 @@
+from . import db
+from flask_login import UserMixin
+from sqlalchemy.sql import func
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(150))
+    polls = db.relationship("Poll")
+
+
+class Candidate(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    poll_id = db.Column(db.Integer, db.ForeignKey('poll.id'))
+    name = db.Column(db.String(150))
+    votes = db.Column(db.Integer)
+
+
+class Poll(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    name = db.Column(db.String(150))
+    status = db.Column(db.String(6))
+    time_created = db.Column(db.DateTime(timezone=True), default=func.now())
+    candidates = db.relationship("Candidate")
+
+
+class ClosedPoll(db.Model):
+    poll_id = db.Column(db.Integer, db.ForeignKey('poll.id'))
+    time_closed = db.Column(db.DateTime(timezone=True), default=func.now())
+
+
+class ActivePoll(db.Model):
+    poll_id = db.Column(db.Integer, db.ForeignKey('poll.id'))
+
+
+class Room(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    key = db.Column(db.String, unique=True)
+    poll_id = db.Column(db.Integer, db.ForeignKey('poll.id'))
