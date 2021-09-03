b4e02f14284f13ff38237c7c8f6b535bc47add4e
MapleLeaves <64523738+MapleLeaves-7@users.noreply.github.com>
2021-09-03 18:26:58 +0800

    Add alerts

--- website/auth.py
+++ website/auth.py
@@ -1,12 +1,11 @@
-from flask import Blueprint
-from flask.templating import render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route("/login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
-    return "<p> login </p>"
+    return render_template("login.html")
 
 
 @auth.route("/logout")
@@ -14,6 +13,29 @@ def lougout():
     return "<p> lougout </p>"
 
 
-@auth.route("/register")
+@auth.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+
+        username = request.form.get("username")
+        if not username:
+            flash("Please enter a username.", category="error")
+
+        password = request.form.get("password")
+        if not password:
+            flash("Please enter a password.", category="error")
+
+        if len(password) < 8:
+            flash("Your password must be at least 8 characters long.",
+                  category="error")
+
+        confirmation = request.form.get("confirmation")
+
+        if not confirmation:
+            flash("You must confirm your password.", category="error")
+        elif password != confirmation:
+            flash("Passwords do not match", category="error")
+        else:
+            flash("Account created!", category="success")
+
     return render_template("register.html")
