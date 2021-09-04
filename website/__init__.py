39e82f7d20d91e2fc55268e02a721ffbf90d2b6c
MapleLeaves <64523738+MapleLeaves-7@users.noreply.github.com>
2021-09-04 21:57:40 +0800

    Remove check for creation of database

--- website/__init__.py
+++ website/__init__.py
@@ -31,7 +31,8 @@ def create_app():
 
     from .models import User
 
-    create_database(app)
+    db.create_all(app)
+    # create_database(app)
 
     login_manager = LoginManager()
     login_manager.login_view = "views.index"
@@ -44,7 +45,7 @@ def create_app():
     return app
 
 
-def create_database(app):
-    if not path.exists('website/' + DB_NAME):
-        db.create_all(app=app)
-        print("Created Database!")
+# def create_database(app):
+#     if not path.exists('website/' + DB_NAME):
+#         db.create_all(app=app)
+#         print("Created Database!")
