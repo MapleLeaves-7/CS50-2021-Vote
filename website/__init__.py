8c40d702a467b32e9027dfac22c0b84a3f9a85a4
MapleLeaves <64523738+MapleLeaves-7@users.noreply.github.com>
2021-09-05 16:48:52 +0800

    Update to use local database

--- website/__init__.py
+++ website/__init__.py
@@ -1,24 +1,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-# from os import path
+from os import path
 from flask_login import LoginManager
 
-import os
-import psycopg2
+# import os
+# import psycopg2
 
-DATABASE_URL = os.environ['DATABASE_URL']
+# DATABASE_URL = os.environ['DATABASE_URL']
 
-conn = psycopg2.connect(DATABASE_URL, sslmode='require')
+# conn = psycopg2.connect(DATABASE_URL, sslmode='require')
 
 db = SQLAlchemy()
-# DB_NAME = "voting.db"
+DB_NAME = "voting.db"
 
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = "asdfasdfasdf asdfasdfasdf"
-    # app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///{DB_NAME}'
-    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://erocdveylmflpx:630e2dacaaeb5e219958ca997f25b5f197ce2eb3503076c2a3ca4f75a1c861fb@ec2-34-193-113-223.compute-1.amazonaws.com:5432/d9gfi6r0ojd0e7"
+    app.config["SQLALCHEMY_DATABASE_URI"] = f'sqlite:///{DB_NAME}'
+    # app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://erocdveylmflpx:630e2dacaaeb5e219958ca997f25b5f197ce2eb3503076c2a3ca4f75a1c861fb@ec2-34-193-113-223.compute-1.amazonaws.com:5432/d9gfi6r0ojd0e7"
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     db.init_app(app)
 
@@ -30,10 +30,10 @@ def create_app():
 
     from .models import User
 
-    with app.app_context():
-        db.create_all()
+    # with app.app_context():
+    # db.create_all()
 
-    # create_database(app)
+    create_database(app)
 
     login_manager = LoginManager()
     login_manager.login_view = "views.index"
@@ -46,7 +46,7 @@ def create_app():
     return app
 
 
-# def create_database(app):
-#     if not path.exists('website/' + DB_NAME):
-#         db.create_all(app=app)
-#         print("Created Database!")
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        db.create_all(app=app)
+        print("Created Database!")
